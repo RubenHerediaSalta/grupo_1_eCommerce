a69040fba895b5aaec9ff7884e398b975286d143
RubenHerediaSalta <ruben.a.her@gmail.com>
2022-04-16 12:40:51 -0300

    modifique el app y un poco de header

--- app.js
+++ app.js
@@ -1,12 +1,29 @@
-const express = require('express');
+const express = require("express");
+const path = require("path");
 const app = express();
-
-app.use(express.static('public'));
+const publicPath = path.resolve(__dirname, "./public");
+app.use(express.static(publicPath));
 
 app.listen(2022 ,()=>{
     console.log('Custom on')
 });
 
-app.get('/register', (req,res)=>{
-    res.sendFile(__dirname + '/views/register.html');
+app.get("/", (req,res)=>{
+    res.sendFile(path.resolve(__dirname, "./views/home.html"))
+})
+
+app.get("/register", (req,res)=>{
+    res.sendFile(__dirname + "/views/register.html");
+});
+
+app.get('/login', (req,res)=>{
+    res.sendFile(__dirname + '/views/login.html');
+});
+
+app.get('/producto', (req,res)=>{
+    res.sendFile(__dirname + '/views/producto.html');
+});
+
+app.get('/carrito', (req,res)=>{
+    res.sendFile(__dirname + '/views/carrito.html');
 });
